86e5e727439ccd037a2c020b7d44bb2ec3110b2c
jskjott <jonathan@jskjott.com>
2019-12-23 16:29:29 +0100

    init commit

---
+++ .eslintrc.js
@@ -0,0 +1,61 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'es6': true,
+		'node': true,
+	},
+	'parser': '@typescript-eslint/parser',
+	'extends': [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/eslint-recommended',
+		'plugin:@typescript-eslint/recommended',
+		'prettier/@typescript-eslint',
+		'plugin:prettier/recommended',
+	],
+	'globals': {
+		'Atomics': 'readonly',
+		'SharedArrayBuffer': 'readonly'
+	},
+	'parserOptions': {
+	    // 'project': './tsconfig.json',
+	},
+	'rules': {
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'no-console': 0,
+		'no-unused-vars': 1,
+		'no-empty': 1,
+		'no-unreachable': 1,
+		'no-constant-condition': 1,
+		'no-implicit-globals': 1,
+		'no-throw-literal': 1,
+		'prefer-promise-reject-errors': 1,
+		'no-extra-bind': 1,
+		'require-atomic-updates': 1,
+		'no-template-curly-in-string': 1,
+		'no-prototype-builtins': 1,
+		'no-misleading-character-class': 1,
+		'no-await-in-loop': 1,
+		'no-async-promise-executor': 1,
+		'no-var': 1,
+		'prefer-rest-params': 1,
+		'prefer-destructuring': 1,
+		'no-useless-call': 1,
+		'prefer-spread': 1,
+		'no-multi-spaces': 1,
+		'no-trailing-spaces': 1,
+		'prettier/prettier': 1,
+		'eqeqeq': 1,
+		'@typescript-eslint/no-var-requires': 0,
+		'@typescript-eslint/no-explicit-any': 0,
+		'@typescript-eslint/explicit-function-return-type': 0,
+		'@typescript-eslint/no-empty-function': 1,
+		'@typescript-eslint/ban-ts-ignore': 0,
+	}
+}
---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+	printWidth: 80,
+	useTabs: true,
+	semi: false,
+	singleQuote: true,
+	quoteProps: 'preserve',
+	trailingComma: 'all',
+	tabWidth: 4,
+}
