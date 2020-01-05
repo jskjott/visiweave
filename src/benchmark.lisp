0ed6c15ec1143b212dc14f3227ef377f7db3442b
jskjott <jonathan@jskjott.com>
2020-01-05 14:16:01 +0000

    add list lambda and map tests

--- src/benchmark.lisp
+++ src/benchmark.lisp
@@ -4,3 +4,11 @@
   (test "sub" (sub 8 4 2) 2)
   (test "mul" (mul 8 4 2) 64)
   (test "div" (div 8 4 2) 1)
+
+; specials
+
+( test "lambda" ((lambda (x) x) "Lisp") "Lisp")
+
+; arrays
+
+(test "map" (map (1 2 3) (lambda (a) (add 1 a))) (2 3 4))
